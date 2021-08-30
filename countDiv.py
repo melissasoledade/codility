027e5c60c0fb22f67911bff86180c060962d22b2
Melissa Soledade <30574287+melissasoledade@users.noreply.github.com>
2021-08-30 17:55:25 -0300

    count div

--- countDiv.py
+++ countDiv.py
@@ -1,14 +1,9 @@
 def solution(A, B, K):
-    array = list(range(A, B+1))
-    div = 0
+    if(A%K == 0):    
+        return (B//K - A//K) + 1
+    else:
+        return (B//K - A//K)
 
-    if(K == 1):
-        return (B-A+1)
-    
-    for i in range(0, len(array)):
-        if(array[i] % K == 0):
-            div = div + 1        
-    return div
-    
+
 if __name__ == '__main__':
-    print(solution(6, 11, 13))
+    print(solution(6, 11,2))
