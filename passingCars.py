fbfa59b5963baa622ec6c3c6c7d247de27441df9
Melissa Soledade <30574287+melissasoledade@users.noreply.github.com>
2021-08-30 15:56:11 -0300

    passing cars

---
+++ passingCars.py
@@ -0,0 +1,28 @@
+def prefix_sums(A):
+    n = len(A)
+    P = [0] * (len(A)+1)
+
+    for i in range(1, len(A)+1):
+        P[i] = P[i-1] + A[i-1]    
+    return P
+
+def count_total(P, x, y):
+    return P[y+1] - P[x]
+
+def solution(A):
+    ones = 0
+    passing = 0
+    for i in range(len(A)-1, -1, -1):
+        if(A[i] == 1):
+            ones = ones + 1
+        else:
+            passing = passing + ones
+    return passing
+
+if __name__ == '__main__':
+    A = [0,1,0,1,1]
+    print(solution(A))
+
+   
+    
+
